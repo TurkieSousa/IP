67df84b4d3fc9cc240663129d25f18b1bd01e3be
TurkieSousa <antoniosousa8e@gmail.com>
2024-12-06 21:23:51 +0000

    gitignore

--- 2400567A.c
+++ 2400567A.c
@@ -3,15 +3,13 @@
 int main() {
     int k, metade;
     
-    // Entrada do valor de K
     printf("Indique K: ");
     scanf("%d", &k);
     
-    // Verificação se K está fora do intervalo permitido e imprime o resultado
     if (k < 2 || k > 100) { 
         printf("K tem de ser entre 2 e 100.\n"); 
     } else {
-        metade = k / 2; // Calcula a metade de K mas o Diogo eh cabecudo
+        metade = k / 2; 
         printf("Sequencia: %d %d\n", metade, metade); 
     }
 }

4baed114921422342fa8b1ab7d7075b320229ba3
TurkieSousa <antoniosousa8e@gmail.com>
2024-12-06 19:40:15 +0000

    feat: ex A

---
+++ 2400567A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int k, metade;
+    
+    // Entrada do valor de K
+    printf("Indique K: ");
+    scanf("%d", &k);
+    
+    // Verificação se K está fora do intervalo permitido e imprime o resultado
+    if (k < 2 || k > 100) { 
+        printf("K tem de ser entre 2 e 100.\n"); 
+    } else {
+        metade = k / 2; // Calcula a metade de K
+        printf("Sequencia: %d %d\n", metade, metade); 
+    }
+}
